8cc510f456d0ff0d1fca625920b91d0b5d602b7c
Lukas Ruebbelke <simpul@gmail.com>
2015-06-23 23:32:31 -0700

    Cleaning up naming

--- angular/app.js
+++ angular/app.js
@@ -27,7 +27,7 @@ angular.module( 'sample', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$routeChangeSuccess', function(e, nextRoute){
     if ( nextRoute.$$route && angular.isDefined( nextRoute.$$route.pageTitle ) ) {
-      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | ngEurope Sample' ;
+      $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Angular Security Sample' ;
     }
   });
 })
