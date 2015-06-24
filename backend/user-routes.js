4fa6d7e6a324da3c7ab92e0880714e66c0dddd33
Lukas Ruebbelke <simpul@gmail.com>
2015-06-24 09:51:25 -0700

    Cleaning up files

--- backend/user-routes.js
+++ backend/user-routes.js
@@ -2,12 +2,11 @@ var express = require('express'),
     _       = require('lodash'),
     config  = require('./config'),
     jwt     = require('jsonwebtoken'),
-    passport = require('passport'),
-    FacebookStrategy = require('passport-facebook').Strategy;
+    passport = require('passport');
 
 var app = module.exports = express.Router();
 
-// XXX: This should be a database of users :).
+// NOTE: This should be a database of users.
 var users = [{
   id: 1,
   username: 'gonto',
@@ -19,8 +18,7 @@ function createToken(user) {
   return jwt.sign(_.omit(user, 'password'), config.secret, { expiresInMinutes: 60*5 });
 }
 
-
-// Regular signup
+// Register
 app.post('/users', function(req, res) {
   if (!req.body.username || !req.body.password) {
     return res.status(400).send("You must send the username and the password");
@@ -40,6 +38,7 @@ app.post('/users', function(req, res) {
   });
 });
 
+// Sign in
 app.post('/sessions/create', function(req, res) {
   if (!req.body.username || !req.body.password) {
     return res.status(400).send("You must send the username and the password");
@@ -58,33 +57,3 @@ app.post('/sessions/create', function(req, res) {
     id_token: createToken(user)
   });
 });
-
-// Facebook signup
-app.get('/auth/facebook', 
-  passport.authenticate('facebook', {session: false}));
-
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {failureRedirect: 'http://localhost:3000/#/#error', session: false}),
-  function(req, res) {
-    var jwt = createToken(req.user);
-    res.redirect('http://localhost:3000/#/#jwt=' + jwt);
-  });
-
-passport.use(new FacebookStrategy({
-    clientID: '1113520895341582',
-    clientSecret: '98075e77f6320447cef79152cfa0cdfc',
-    callbackURL: "http://localhost:3001/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    
-    var user = {
-      username: profile.displayName,
-      provider: 'facebook',
-      token: accessToken
-    };
-    user.id = _.max(users, 'id').id + 1;
-    users.push(user);
-    done(null, user);
-  }
-));
-
